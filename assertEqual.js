94903804953663e508ba3815b638e8b2d1d00447
Alexander Da Costa <bsmuse@tutamail.com>
2023-09-19 11:48:10 -0600

    Made assertEqual file

---
+++ assertEqual.js
@@ -0,0 +1,18 @@
+// Make the function compare the two values it takes in and print out a message telling us if they match or not.
+
+// If the values match, it should print (console.log) the following: Assertion Passed: [actual] === [expected] (but with the values filled in)
+// Otherwise it should print (console.log) the following: Assertion Failed: [actual] !== [expected] (but with the values filled in)
+
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+      console.assert(actual === expected, `${actual} ! == ${expected}`) ;
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
