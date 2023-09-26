32b53e10c9b4f98f3d9ba5d59d42c9b06d970b1e
Alexander Da Costa <bsmuse@tutamail.com>
2023-09-26 09:52:31 -0600

    Add assertArraysTest.js file and refactor assertArray.js for test

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,24 +1,9 @@
-let answer = true;
-
-const eqArrays = (array1, array2) => {
-  if (array1.length != array2.length){
-    answer = false;
-  } else {
-      for (let i = 0; i < array1.length; i++) {
-        if (array1[i] !== array2[i]) {
-          answer = false;
-        }
-      }
-    }
-    return answer;
-};
-
-const assertArraysEqual = () => {
+const assertArraysEqual = (answer) => {
   if (answer === true) {
     console.log('Both arrays are equal');
   } else {
     console.log('Arrays are not equal');
   }
-} 
+}  
 
-assertArraysEqual(eqArrays([1,2,3], [1,2,3]));
+module.exports = assertArraysEqual;
