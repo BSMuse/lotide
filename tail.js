c75d5570cae56b82cb98c6de9a880e4563699f8c
Alexander Da Costa <bsmuse@tutamail.com>
2023-09-26 09:17:30 -0600

    Add tail test and refactor tail.js file for rests

--- tail.js
+++ tail.js
@@ -1,14 +1,5 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-      console.assert(actual === expected, `${actual} ! == ${expected}`) ;
-  }
-}; 
-
 const tail = (array) => {
   return array.slice(1).toString();
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]).toString();
-assertEqual(result, ["Lighthouse", "Labs"].toString());
+module.exports = tail;
